19e6591fe0d3ac8e7c744f5794800a397fd9a6e6
ansel brandt <ansel@anselbrandt.com>
2023-10-09 23:02:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vanilla JS Pull To Refresh
+
+https://nerdy.dev/pull-to-refresh-prototype-with-scroll-snap-and-scroll-driven-animation
+
+https://codepen.io/argyleink/pen/ExOWjMe
+
+https://github.com/anselbrandt/vanilla-pull-to-refresh
+
+https://www.npmjs.com/package/scrollyfills
---
+++ main.js
@@ -0,0 +1,36 @@
+import "scrollyfills";
+import "./style.css";
+
+const ptr_scrollport = document.querySelector("html");
+const ptr = document.querySelector("#refresh");
+const main = document.querySelector("#refresh-main");
+
+const determinePTR = (event) => {
+  if (event.target.scrollTop <= 0) {
+    // fetch()
+    ptr.querySelector("span").textContent = "refreshing...";
+    ptr.setAttribute("loading-state", "loading");
+
+    // sim response
+    setTimeout(() => {
+      ptr.querySelector("span").textContent = "done!";
+
+      setTimeout(() => {
+        ptr.removeAttribute("loading-state");
+        main.scrollIntoView({ behavior: "smooth" });
+
+        window.addEventListener(
+          "scrollend",
+          (e) => {
+            ptr.querySelector("span").textContent = "Pull to refresh";
+          },
+          { once: true }
+        );
+      }, 500);
+    }, 2000);
+  }
+};
+
+window.addEventListener("scrollend", (e) => {
+  determinePTR({ target: ptr_scrollport });
+});
